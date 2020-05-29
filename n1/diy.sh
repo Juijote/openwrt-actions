61032a4b2f9957b5250b20b7cbe4f3584dce1df0
Juijote <cgjfeng@gmail.com>
2020-05-29 16:40:29 +0800

    Update diy.sh

--- n1/diy.sh
+++ n1/diy.sh
@@ -1,1 +1,5 @@
+#echo '修改网关地址'
 sed -i 's/192.168.1.10/192.168.1.2/g' package/base-files/files/bin/config_generate
+
+echo '修改默认主题'
+sed -i 's/config internal themes/config internal themes\n    option Argon  \"\/luci-static\/argon\"/g' feeds/luci/modules/luci-base/root/etc/config/luci
